83f72a94adbd59a356498138e8c47adaffe56442
dankarger1 <karger.dan@gmail.com>
2023-11-23 20:46:09 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
